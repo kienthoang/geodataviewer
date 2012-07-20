0c3e7b49fbdd535bf3162624103de09f5930d2f0
Kien Hoang <hoangk@lafayette.edu>
2012-07-19 23:51:03 -0400

    Store database in a different dir now => Hidden from Itunes.

--- GeoFieldBook/GeoDatabaseManager.m
+++ GeoFieldBook/GeoDatabaseManager.m
@@ -68,9 +68,11 @@ static GeoDatabaseManager *standardDatabaseManager;
 - (void)synchronizeWithMainDatabase {
     //If the main database doesn't exist on disk yet, create it
     NSFileManager *fileManager=[NSFileManager defaultManager];
-    NSURL *documentDirURL=[[fileManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
-    NSURL *databaseURL=[documentDirURL URLByAppendingPathComponent:DATABASE_FILE_LOCALIZED_NAME];
-    if (![fileManager fileExistsAtPath:[databaseURL path]]) {
+    NSURL *appSupportDirURL=[fileManager URLsForDirectory:NSApplicationSupportDirectory inDomains:NSUserDomainMask].lastObject;
+    if (![fileManager fileExistsAtPath:appSupportDirURL.path])
+        [fileManager createDirectoryAtPath:appSupportDirURL.path withIntermediateDirectories:YES attributes:nil error:NULL];
+    NSURL *databaseURL=[appSupportDirURL URLByAppendingPathComponent:DATABASE_FILE_LOCALIZED_NAME];
+    if (![fileManager fileExistsAtPath:databaseURL.path]) {
         //Create the main database
         [self createMainDatabaseWithURL:databaseURL];
     }
